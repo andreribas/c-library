6f264c75bdc10a8f2107bc4f47941c1474a97251
André Ribas <andre.ribas@ebanx.com>
2020-11-29 17:13:50 -0300

    Add create Header button

--- templates/header/index.php
+++ templates/header/index.php
@@ -5,6 +5,12 @@
 <?php block_set('title', "Headers"); ?>
 
 <?php block_start('content'); ?>
+    <div class="float-right">
+        <form action="/headers/create" method="get">
+            <button type="submit" class="btn btn-primary">Create new Header</button>
+        </form>
+    </div>
+
     <h1>Headers</h1>
     <?php template_include('partial/list_headers.php', ['functionns' => $headers]); ?>
 <?php block_end(); ?>
