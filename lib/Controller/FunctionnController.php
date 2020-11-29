511bd4724252a91d3b06cb20f8f0e9052d9e4666
André Ribas <andre.ribas@ebanx.com>
2020-11-29 16:28:40 -0300

    Add create function action

--- lib/Controller/FunctionnController.php
+++ lib/Controller/FunctionnController.php
@@ -4,6 +4,7 @@ namespace AndreRibas\Clibrary\Controller;
 
 use AndreRibas\Clibrary\App\Request;
 use AndreRibas\Clibrary\App\Response;
+use AndreRibas\Clibrary\Model\Functionn;
 use AndreRibas\Clibrary\Repository\FunctionnRepository;
 use AndreRibas\Clibrary\Repository\HeaderRepository;
 
@@ -18,12 +19,20 @@ class FunctionnController
 
     public static function create(Request $request)
     {
-
+        return new Response('functionn/create.php', [
+            'headers' => HeaderRepository::getAll(),
+        ]);
     }
 
     public static function store(Request $request)
     {
+        $functionn = new Functionn();
+        $functionn->title = $request->getParam('title');
+        $functionn->description = $request->getParam('description');
+        $functionn->header_id = (int) $request->getParam('header_id');
+        $functionn->id = FunctionnRepository::create($functionn);
 
+        return self::show($request, $functionn->id);
     }
 
     public static function show(Request $request, $functionn_id)
--- templates/functionn/create.php
+++ templates/functionn/create.php
@@ -1,1 +1,34 @@
-<?php
+<?php /** @var \AndreRibas\Clibrary\Model\Header[] $headers */ ?>
+
+<?php template_extend('base_template.php'); ?>
+
+<?php block_set('title', "Create Function"); ?>
+
+<?php block_start('content'); ?>
+    <form method="post" action="/functions">
+
+        <h2>Create Function</h2>
+
+        <div class="form-group">
+            <label for="functionTitle">Function Title</label>
+            <input type="text" class="form-control" id="functionTitle" name="title">
+        </div>
+
+        <div class="form-group">
+            <label for="functionHeader">Header</label>
+            <select id="functionHeader" class="form-control" name="header_id">
+                <?php foreach ($headers as $header): ?>
+                    <option value="<?= $header->id ?>"><?= $header->title ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="functionDescription">Function Description</label>
+            <textarea class="form-control" id="functionDescription" name="description"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    </form>
+<?php block_end(); ?>
